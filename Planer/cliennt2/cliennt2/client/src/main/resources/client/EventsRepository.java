de7b3f9bf921b6cb28e037d7221eafec5a51d069
Lyana2021 <84101307+Lyana2021@users.noreply.github.com>
2022-04-12 17:40:18 +0300

    Delete Planer/cliennt2/cliennt2 directory

--- Planer/cliennt2/cliennt2/client/src/main/resources/client/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface EventsRepository extends JpaRepository<Event,Long> {
-    List<Event> findByLocation(String location);
-}
