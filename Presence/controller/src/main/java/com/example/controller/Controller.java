6bc79d684990ba0b5b6da97d2bd4dd564040499f
anna5812m <anna5812m@yandex.ru>
2022-04-17 02:19:24 +0300

    Оставшиеся задачи: 1. Решить проблему с подчестом записей. 2. Скрыть поле с комментарием и кнопкой "Я здесь" (создавалось для заполнения БД).

--- Presence/controller/src/main/java/com/example/controller/Controller.java
+++ Presence/controller/src/main/java/com/example/controller/Controller.java
@@ -5,7 +5,6 @@ import com.example.controller.repos.MessRep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -18,7 +17,13 @@ public class Controller {
     final Random random = new Random();
     @Autowired
     private MessRep messRep;
-    public String number;
+
+  /*  @GetMapping("api/presence/send")
+    public String presence(Map<String, Object> model) {
+        Iterable<Message> messages = messRep.findAll();
+        model.put("messages", messages);
+        return "presence";
+    }*/
 
     @GetMapping("api/getNameTopic")
     public String presence(Map<String, Object> model) {
@@ -28,22 +33,25 @@ public class Controller {
     }
 
 
-  /* @PostMapping("api/presence/send")
-    public String send(Integer id_event, String topicName, Map<String, Object> model) {
-        id_event_rand(id_event);
-        topicName_rand(topicName);
-        Message message = new Message(id_event, topicName);
+   @PostMapping("api/presence/send")
+    public String send(@RequestParam(name = "comment", required = false) String comment, String event, String topicName, String visitor, Map<String, Object> model) {
+        int rand = random.nextInt(5);
+        event = "event" + String.valueOf(rand);
+        topicName = "topic" + String.valueOf(rand);
+        visitor = "visitor";
+        Message message = new Message(comment, event, topicName, visitor);
         messRep.save(message);
         Iterable<Message> messages = messRep.findAll();
         model.put("messages", messages);
         return "presence";
-    }*/
+    }
 
    @PostMapping("api/getNameTopic")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
+    public String filter(@RequestParam String filter, Map<String, Object> model, Integer amount){
         Iterable<Message> messages;
         if (filter != null && !filter.isEmpty()) {
             messages = messRep.findByTopicName(filter);
+            amount = ((List<Message>) messages).size();
         } else {
             messages = messRep.findAll();
         }
@@ -52,7 +60,7 @@ public class Controller {
     }
 
 
- /*   public Integer id_event_rand(Integer id_event) {
+  /* public Integer id_event_rand(Integer id_event) {
         id_event = random.nextInt(10);
         return id_event;
     }
--- Presence/controller/src/main/java/com/example/controller/ControllerApplication.java
+++ Presence/controller/src/main/java/com/example/controller/ControllerApplication.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import com.samskivert.mustache.Mustache;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,5 +17,17 @@ public class ControllerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControllerApplication.class, args);
+	}
+
+	// Переопределить MustacheAutoConfiguration для поддержки defaultValue ("")
+	@Bean
+	public Mustache.Compiler mustacheCompiler(Mustache.TemplateLoader mustacheTemplateLoader, Environment environment) {
+		MustacheEnvironmentCollector collector = new MustacheEnvironmentCollector();
+		collector.setEnvironment(environment);
+	// значение по умолчанию
+		Mustache.Compiler compiler = Mustache.compiler().defaultValue("")
+				.withLoader(mustacheTemplateLoader)
+				.withCollector(collector);
+		return compiler;
 	}
 }
