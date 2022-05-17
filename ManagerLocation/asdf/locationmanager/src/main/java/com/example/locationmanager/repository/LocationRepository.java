fd413c46fb08715ae731cfc548e2889b13f3ea32
K418 <76588508+K418@users.noreply.github.com>
2022-05-17 18:27:47 +0300

    Delete ManagerLocation/asdf directory

--- ManagerLocation/asdf/locationmanager/src/main/java/com/example/locationmanager/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.locationmanager.repository;
-
-import com.example.locationmanager.model.Location;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LocationRepository extends CrudRepository<Location, String> {
-
-}
