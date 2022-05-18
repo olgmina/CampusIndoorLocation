0e71ec0ff6df2543ee5afa403572ffd9a5bda005
Alexandr-Brazhnikov <82893287+Alexandr-Brazhnikov@users.noreply.github.com>
2022-05-18 16:50:21 +0300

    Delete WorkLess/src directory

--- WorkLess/src/main/java/com/WorkLess/WorkLess/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.WorkLess.WorkLess.repository;
-
-import com.WorkLess.WorkLess.model.Schedule;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ScheduleRepository extends CrudRepository<Schedule, String> {
-}
