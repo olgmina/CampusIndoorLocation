681a95cf8e1d8a840255ee309f927c087216325b
Антон <80773270+AnathisT@users.noreply.github.com>
2022-05-27 09:26:37 +0300

    Delete Auditor/Project directory

--- Auditor/Project/AdminServer/src/main/java/com/example/adminserver/reposutoryes/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.adminserver.reposutoryes;
-
-import com.example.adminserver.models.Organizational;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface UsersRepository extends JpaRepositoryImplementation <Organizational,Long> {
-}
